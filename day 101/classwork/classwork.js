98d5e342da3e71c7d151e1ccad44bef295baefba
demetregorgiashvili <gorgiashvilidemetre@gmail.com>
2026-02-06 22:06:18 +0400

    commit

---
+++ day 101/classwork/classwork.js
@@ -0,0 +1,27 @@
+//classwork 5
+
+let tempet = 10;
+
+if (tempet<15){
+    console.log("its cold outside")
+} else if (25>tempet>15){
+    console.log("its normal outside")
+} else {
+    console.log("its hot outside")
+}
+
+//classwork 6
+
+let a = 1
+
+let b = 2
+
+let c = 3
+
+if (a>b>c) {
+    console.log ("a is biggest")
+} else if (b>a>c) {
+    console.log ("b is biggest")
+} else {
+    console.log ("c is biggest")
+}
