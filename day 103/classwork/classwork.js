df2a4321f9fd439694d661915d87f00debb245fa
demetregorgiashvili <gorgiashvilidemetre@gmail.com>
2026-02-13 17:39:37 +0400

    commit

---
+++ day 103/classwork/classwork.js
@@ -0,0 +1,9 @@
+function name(name){
+    if (name==="demetre"){
+        console.log("We have same name")
+    } else {
+        console.log("we dont have same name")
+    }
+}
+
+let numbers = (a,b,c) => a + b + c ;
---
+++ day 104/classwork/classwork.js
@@ -0,0 +1,17 @@
+const Odd_Or_Eaven = function() {
+    if (Odd_Or_Eaven%2===0){
+        console.log("your number is even")
+} else {
+    console.log("your number is Odd")
+}
+}
+
+function Hello(name) {
+    if (name===""," "){
+        console.log("Hello Guest")
+
+    } else {
+        console.log("hello"+name)
+    }
+}
+
